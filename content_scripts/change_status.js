64459a643c0570606d6710d3322f085feaec9065
Drew Rutherford <andrew.rutherford89@gmail.com>
2024-07-18 14:14:56 -0400

    4.6.3 Release Notes: - Status now changes to Backlog instead of Offline. (Omni fixed their routing... hopefully)

--- content_scripts/change_status.js
+++ content_scripts/change_status.js
@@ -269,8 +269,7 @@
                 changeToAvailable();
             }
             else if (items.savedStartShift <= currentTime && items.savedEndShift >= currentTime) {
-                changeToOffline()
-                //normally this would be changeToBacklog() but the queue system is configured incorrectly and causing case balancing issues
+                changeToBacklog()
             }
             else {
                 changeToOffline();
